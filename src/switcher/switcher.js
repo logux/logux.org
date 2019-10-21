04ebe71df8cbb7eee198e7016385755876bd299c
Andrey Sitnik <andrey@sitnik.ru>
2019-10-22 01:49:55 +0900

    Improve switcher keyboard support

--- src/switcher/switcher.js
+++ src/switcher/switcher.js
@@ -3,8 +3,8 @@ let switchers = document.querySelectorAll('.switcher')
 for (let switcher of switchers) {
   let tabs = switcher.children[0]
   let sections = Array.from(switcher.children).slice(1)
-  let lastOpen = sections[0]
-  let lastButton = tabs.children[0].children[0]
+  let lastSection = sections[0]
+  let lastTab = tabs.children[0].children[0]
 
   let byId = sections.reduce((all, section) => {
     all[section.getAttribute('aria-labelledby')] = section
@@ -14,11 +14,33 @@ for (let switcher of switchers) {
   tabs.addEventListener('click', e => {
     if (e.target.tagName !== 'BUTTON') return
     let section = byId[e.target.id]
-    lastButton.removeAttribute('aria-selected', false)
+    lastTab.removeAttribute('aria-selected', false)
     e.target.setAttribute('aria-selected', 'true')
-    lastOpen.setAttribute('hidden', true)
+    lastTab.setAttribute('tabindex', '-1')
+    e.target.removeAttribute('tabindex')
+    lastSection.setAttribute('hidden', true)
     section.removeAttribute('hidden', false)
-    lastOpen = section
-    lastButton = e.target
+    lastSection = section
+    lastTab = e.target
+  })
+
+  tabs.addEventListener('keydown', e => {
+    let next
+    if (e.code === 'ArrowRight') {
+      next = lastTab.parentNode.nextSibling
+      if (!next) next = tabs.children[0]
+    } else if (e.code === 'ArrowLeft') {
+      next = lastTab.parentNode.previousSibling
+      if (!next) next = tabs.children[tabs.children.length - 1]
+    } else if (e.code === 'ArrowDown') {
+      e.preventDefault()
+      lastSection.focus()
+    }
+    if (next) {
+      e.preventDefault()
+      let nextTab = next.children[0]
+      nextTab.focus()
+      nextTab.click()
+    }
   })
 }
