b9d7b866bb17977b9c005578edd45fcf46d355ff
Andrey Sitnik <andrey@sitnik.ru>
2019-10-23 16:58:14 +0900

    Clean up code

--- src/switcher/switcher.js
+++ src/switcher/switcher.js
@@ -1,81 +1,118 @@
 let switchers = document.querySelectorAll('.switcher')
 
-let byValue = { }
+function keepScroll (el, cb) {
+  let prevTop = el.getBoundingClientRect().top
+  cb()
+  let diff = el.getBoundingClientRect().top - prevTop
+  window.scrollTo(window.scrollX, window.scrollY + diff)
+}
+
+let recursionBlocked = false
+function preventRecursion (cb) {
+  if (!recursionBlocked) {
+    recursionBlocked = true
+    cb()
+    recursionBlocked = false
+  }
+}
+
+function changeTab (from, to) {
+  from.removeAttribute('aria-selected')
+  to.setAttribute('aria-selected', 'true')
+  from.setAttribute('tabindex', '-1')
+  to.removeAttribute('tabindex')
+  return to
+}
+
+function changeSection (from, to) {
+  from.setAttribute('hidden', true)
+  to.removeAttribute('hidden')
+  return to
+}
+
+function focusOnce (el) {
+  el.setAttribute('tabindex', '-1')
+  function listener () {
+    el.removeAttribute('tabindex')
+    el.removeEventListener('blur', listener)
+  }
+  el.addEventListener('blur', listener)
+  el.focus()
+}
+
+function openTab (tab) {
+  tab.focus()
+  tab.click()
+}
+
+function onKey (el, keys) {
+  el.addEventListener('keydown', e => {
+    if (keys[e.code]) {
+      e.preventDefault()
+      keys[e.code]()
+    }
+  })
+}
+
+function mapByAttr (els, attr) {
+  return els.reduce((all, section) => {
+    all[section.getAttribute(attr)] = section
+    return all
+  }, { })
+}
+
+function addToGroupByText (group, els) {
+  for (let i of els) {
+    let value = i.innerText
+    if (!group[value]) group[value] = []
+    group[value].push(i)
+  }
+}
+
+let byText = { }
 
 for (let switcher of switchers) {
   let tabs = switcher.children[0]
+  let firstTab = tabs.children[0]
+  let lastTab = tabs.children[tabs.children.length - 1]
+  let currentTab = firstTab
+
   let sections = Array.from(switcher.children).slice(1)
-  let lastSection = sections[0]
-  let lastTab = tabs.children[0]
+  let currentSection = sections[0]
 
-  let byId = sections.reduce((all, section) => {
-    all[section.getAttribute('aria-labelledby')] = section
-    return all
-  }, { })
+  addToGroupByText(byText, tabs.children)
+  let byId = mapByAttr(sections, 'aria-labelledby')
 
-  let groupChanging = false
   tabs.addEventListener('click', e => {
     if (e.target.tagName !== 'BUTTON') return
-    if (e.target === lastTab) return
+    if (e.target === currentTab) return
 
-    let prevTop = e.target.getBoundingClientRect().top
-    let section = byId[e.target.id]
-    lastTab.removeAttribute('aria-selected', false)
-    e.target.setAttribute('aria-selected', 'true')
-    lastTab.setAttribute('tabindex', '-1')
-    e.target.removeAttribute('tabindex')
-    lastSection.setAttribute('hidden', true)
-    section.removeAttribute('hidden', false)
-    lastSection = section
-    lastTab = e.target
-
-    if (!groupChanging) {
-      groupChanging = true
-      for (let i of byValue[e.target.innerText]) {
-        if (!i.hasAttribute('aria-selected')) {
-          i.click()
+    keepScroll(e.target, () => {
+      currentTab = changeTab(currentTab, e.target)
+      currentSection = changeSection(currentSection, byId[currentTab.id])
+      preventRecursion(() => {
+        for (let i of byText[currentTab.innerText]) {
+          if (i !== currentTab) i.click()
         }
-      }
-      groupChanging = false
-    }
-
-    let diff = e.target.getBoundingClientRect().top - prevTop
-    window.scrollTo(window.scrollX, window.scrollY + diff)
-  })
-
-  tabs.addEventListener('keydown', e => {
-    let nextTab
-    if (e.code === 'Home') {
-      nextTab = tabs.children[0]
-    } else if (e.code === 'End') {
-      nextTab = tabs.children[tabs.children.length - 1]
-    } else if (e.code === 'ArrowRight') {
-      nextTab = lastTab.nextSibling
-      if (!nextTab) nextTab = tabs.children[0]
-    } else if (e.code === 'ArrowLeft') {
-      nextTab = lastTab.previousSibling
-      if (!nextTab) nextTab = tabs.children[tabs.children.length - 1]
-    } else if (e.code === 'ArrowDown') {
-      e.preventDefault()
-      lastSection.setAttribute('tabindex', '-1')
-      lastSection.focus()
-    }
-    if (nextTab) {
-      e.preventDefault()
-      nextTab.focus()
-      nextTab.click()
-    }
-  })
-
-  for (let section of sections) {
-    section.addEventListener('blur', e => {
-      e.target.removeAttribute('tabindex')
+      })
     })
-  }
+  })
 
-  for (let tab of tabs.children) {
-    let value = tab.innerText
-    if (!byValue[value]) byValue[value] = []
-    byValue[value].push(tab)
-  }
+  onKey(tabs, {
+    ArrowDown () {
+      focusOnce(currentSection)
+    },
+    ArrowRight () {
+      openTab(currentTab.nextSibling || firstTab)
+    },
+    ArrowLeft () {
+      openTab(currentTab.previousSibling || lastTab)
+    },
+    Home () {
+      openTab(firstTab)
+    },
+    End () {
+      openTab(lastTab)
+    }
+  })
 }
