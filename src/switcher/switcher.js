69fe99bdd5b91805be610746ee1259fed80ee0fb
Andrey Sitnik <andrey@sitnik.ru>
2019-10-25 13:16:53 +0900

    Speed up switcher

--- src/switcher/switcher.js
+++ src/switcher/switcher.js
@@ -1,19 +1,8 @@
-let switchers = document.querySelectorAll('.switcher')
-
 function keepScroll (el, cb) {
   let prevTop = el.getBoundingClientRect().top
   cb()
   let diff = el.getBoundingClientRect().top - prevTop
   window.scrollTo(window.scrollX, window.scrollY + diff)
-}
-
-let recursionBlocked = false
-function preventRecursion (cb) {
-  if (!recursionBlocked) {
-    recursionBlocked = true
-    cb()
-    recursionBlocked = false
-  }
 }
 
 function changeTab (from, to) {
@@ -40,7 +29,7 @@ function focusOnce (el) {
   el.focus()
 }
 
-function openTab (tab) {
+function clickOnTab (tab) {
   tab.focus()
   tab.click()
 }
@@ -54,14 +43,7 @@ function onKey (el, keys) {
   })
 }
 
-function mapByAttr (els, attr) {
-  return els.reduce((all, section) => {
-    all[section.getAttribute(attr)] = section
-    return all
-  }, { })
-}
-
-function addToGroupByText (group, els) {
+function addToGroupByValue (group, els) {
   for (let i of els) {
     let value = i.innerText
     if (!group[value]) group[value] = []
@@ -69,8 +51,10 @@ function addToGroupByText (group, els) {
   }
 }
 
-let byText = { }
+let byValue = { }
+let open = new Map()
 
+let switchers = document.querySelectorAll('.switcher')
 for (let switcher of switchers) {
   let tabs = switcher.children[0]
   let firstTab = tabs.children[0]
@@ -80,21 +64,25 @@ for (let switcher of switchers) {
   let sections = Array.from(switcher.children).slice(1)
   let currentSection = sections[0]
 
-  addToGroupByText(byText, tabs.children)
-  let byId = mapByAttr(sections, 'aria-labelledby')
+  addToGroupByValue(byValue, tabs.children)
+
+  for (let tab of tabs.children) {
+    let section = sections.find(i => {
+      return i.getAttribute('aria-labelledby') === tab.id
+    })
+    open.set(tab, () => {
+      currentTab = changeTab(currentTab, tab)
+      currentSection = changeSection(currentSection, section)
+    })
+  }
 
   tabs.addEventListener('click', e => {
     if (e.target.tagName !== 'BUTTON') return
     if (e.target === currentTab) return
 
     keepScroll(e.target, () => {
-      currentTab = changeTab(currentTab, e.target)
-      currentSection = changeSection(currentSection, byId[currentTab.id])
-      preventRecursion(() => {
-        for (let i of byText[currentTab.innerText]) {
-          if (i !== currentTab) i.click()
-        }
-      })
+      open.get(e.target)()
+      for (let i of byValue[currentTab.innerText]) open.get(i)()
     })
   })
 
@@ -103,16 +91,16 @@ for (let switcher of switchers) {
       focusOnce(currentSection)
     },
     ArrowRight () {
-      openTab(currentTab.nextSibling || firstTab)
+      clickOnTab(currentTab.nextSibling || firstTab)
     },
     ArrowLeft () {
-      openTab(currentTab.previousSibling || lastTab)
+      clickOnTab(currentTab.previousSibling || lastTab)
     },
     Home () {
-      openTab(firstTab)
+      clickOnTab(firstTab)
     },
     End () {
-      openTab(lastTab)
+      clickOnTab(lastTab)
     }
   })
 }
