498ed4cd5ff5bf92d826f8fa76931e376e8b867e
Andrey Sitnik <andrey@sitnik.ru>
2019-11-03 21:50:06 -0500

    Remove script crossOrigin

--- src/chat/chat.js
+++ src/chat/chat.js
@@ -17,7 +17,6 @@ function initChat () {
     let script = document.createElement('script')
     script.src = 'https://sidecar.gitter.im/dist/sidecar.v1.js'
     script.async = true
-    script.crossOrigin = ''
     document.head.appendChild(script)
   }
 }
