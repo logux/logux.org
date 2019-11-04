d8773728d5a919e29e3b29ed6f9cbfaa64e37a05
Andrey Sitnik <andrey@sitnik.ru>
2019-11-04 10:12:24 -0500

    Repack scripts with Rollup

---
+++ scripts/steps/repack-scripts.js
@@ -0,0 +1,20 @@
+let { writeFile } = require('fs').promises
+let { terser } = require('rollup-plugin-terser')
+let { rollup } = require('rollup')
+let { join } = require('path')
+
+const SRC = join(__dirname, '..', '..', 'src')
+
+module.exports = async function repackScripts (assets) {
+  let scripts = assets.get(/\.js$/).map(compiled => {
+    let name = compiled.replace(/^.+[^\w](\w+)\.[\w\d]+\.js$/, '$1.js')
+    return [join(SRC, name), compiled]
+  })
+  await Promise.all(scripts.map(async ([input, output]) => {
+    let bundle = await rollup({
+      input, plugins: [terser()]
+    })
+    let results = await bundle.generate({ format: 'esm' })
+    await writeFile(output, results.output[0].code)
+  }))
+}
