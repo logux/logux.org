dcc81fe83752fce9eb119f9b7d91c1ed0505b2f9
Andrey Sitnik <andrey@sitnik.ru>
2019-11-06 13:07:49 -0500

    Remove dark switcher

--- src/uikit.js
+++
@@ -1,9 +0,0 @@
-let htmlClasses = document.documentElement.classList
-
-function processHash () {
-  htmlClasses.toggle('is-light', location.hash === '#light')
-  htmlClasses.toggle('is-dark', location.hash === '#dark')
-}
-
-window.addEventListener('hashchange', processHash)
-processHash()
