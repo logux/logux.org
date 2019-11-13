d1dcfd5065bf5aa0a06fada453bb13b5e90b7c78
Andrey Sitnik <andrey@sitnik.ru>
2019-11-13 10:01:22 -0800

    Trim unnecessary classes from server API

--- scripts/steps/read-jsdoc.js
+++ scripts/steps/read-jsdoc.js
@@ -4,16 +4,31 @@ let globby = require('globby')
 
 const PROJECTS = join(__dirname, '..', '..', '..')
 
+function trim (tree, classes) {
+  return tree.filter(i => {
+    return i.kind !== 'class' || !classes.includes(i.name)
+  })
+}
+
 module.exports = async function readJsdoc (spin, ...projects) {
   spin.add(`jsdoc${ projects.join() }`, { text: 'Generating JSDoc' })
   let files = await Promise.all(projects.map(i => {
+    let ignore = []
+    if (i === 'logux-core' && projects.includes('logux-server')) {
+      ignore = [
+        'reconnect.js', 'logux-error.js', 'client-node.js', 'ws-connection.js'
+      ]
+    }
     return globby('**/*.js', {
       absolute: true,
-      ignore: ['node_modules', 'test', 'coverage'],
+      ignore: ['node_modules', 'test', 'coverage', ...ignore],
       cwd: join(PROJECTS, i)
     })
   }))
   let tree = await build(files.flat(), { hljs: { languages: ['js'] } })
+  if (projects.includes('logux-server')) {
+    tree = trim(tree, ['Reconnect', 'LoguxError', 'ClientNode', 'WsConnection'])
+  }
   spin.succeed(`jsdoc${ projects.join() }`, { text: 'JSDoc generated' })
   return tree
 }
