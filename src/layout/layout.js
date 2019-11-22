0aa479675862a6f6ec32fd277c442c8b38a57522
Andrey Sitnik <andrey@sitnik.ru>
2019-11-22 00:50:06 -0500

    Fix class name

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -5,14 +5,14 @@ let aside = document.querySelector('.layout_aside')
 
 if (open && close && aside) {
   open.addEventListener('click', () => {
-    document.body.classList.add('is-no-scroll')
+    document.body.classList.add('has-no-scroll')
     aside.classList.add('is-open')
     open.setAttribute('hidden', true)
     chat.setAttribute('hidden', true)
     close.removeAttribute('hidden')
   })
   close.addEventListener('click', () => {
-    document.body.classList.remove('is-no-scroll')
+    document.body.classList.remove('has-no-scroll')
     aside.classList.remove('is-open')
     open.removeAttribute('hidden')
     chat.removeAttribute('hidden')
