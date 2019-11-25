0aeb4fdadef034ef6ef3086000c77fae71d5540e
Andrey Sitnik <andrey@sitnik.ru>
2019-11-25 00:19:16 -0500

    Close mobile menu on hash-link

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,10 +1,17 @@
-let open = document.querySelector('.layout_open')
-let close = document.querySelector('.layout_close')
-let chat = document.querySelector('.layout_chat')
-let aside = document.querySelector('.layout_aside')
+function isSimpleClick (e) {
+  return e.button === 0 && !e.metaKey && !e.ctrlKey && !e.shiftKey && !e.altKey
+}
 
+let aside = document.querySelector('.layout_aside')
+let links = document.querySelectorAll('.layout_aside a[href^="#"]')
+let close = document.querySelector('.layout_close')
+let open = document.querySelector('.layout_open')
+let chat = document.querySelector('.layout_chat')
+
+let closed = true
 if (open && close && aside) {
   open.addEventListener('click', () => {
+    closed = false
     document.body.classList.add('has-no-scroll')
     aside.classList.add('is-open')
     open.setAttribute('hidden', true)
@@ -12,10 +19,17 @@ if (open && close && aside) {
     close.removeAttribute('hidden')
   })
   close.addEventListener('click', () => {
+    closed = true
     document.body.classList.remove('has-no-scroll')
     aside.classList.remove('is-open')
     open.removeAttribute('hidden')
     chat.removeAttribute('hidden')
     close.setAttribute('hidden', true)
+  })
+}
+
+for (let link of links) {
+  link.addEventListener('click', e => {
+    if (!closed && isSimpleClick(e)) close.click()
   })
 }
