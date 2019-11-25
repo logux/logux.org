42245a9d0632bf6a0af01983c3d4f2cc566e1b66
Andrey Sitnik <andrey@sitnik.ru>
2019-11-25 18:56:28 -0500

    Fix Kurbanistika loading by logotype instead of just logo

--- src/common.js
+++ src/common.js
@@ -1,3 +1,4 @@
 import './base/base.js'
 import './layout/layout.js'
 import './chat/chat.js'
+import './menu/menu.js'
---
+++ src/menu/menu.js
@@ -0,0 +1,5 @@
+if (document.fonts) {
+  window.addEventListener('load', () => {
+    document.fonts.load('10pt Kurbanistika')
+  })
+}
