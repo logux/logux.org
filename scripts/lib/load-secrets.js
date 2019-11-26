1f3bf630547fe4d5d44431e73dc9c3e73f68e514
Andrey Sitnik <andrey@sitnik.ru>
2019-11-25 23:46:26 -0500

    Fix secrets with GitHub Actions

--- scripts/lib/load-secrets.js
+++ scripts/lib/load-secrets.js
@@ -6,6 +6,14 @@ import { ROOT } from './dirs.js'
 let cache
 
 export default async function loadSecrets () {
+  if (process.env.GITTER_TOKEN) {
+    return {
+      gitter: {
+        roomId: process.env.GITTER_ROOM_ID,
+        token: process.env.GITTER_TOKEN
+      }
+    }
+  }
   if (!cache) {
     cache = JSON.parse(await fs.readFile(join(ROOT, 'secrets.json')))
   }
