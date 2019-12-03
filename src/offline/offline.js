6781106fc8feef8d7f277d61319090b680f637a7
Andrey Sitnik <andrey@sitnik.ru>
2019-12-03 07:03:46 -0500

    Cache data on mobile

--- src/offline/offline.js
+++ src/offline/offline.js
@@ -1,7 +1,18 @@
 import isSaveDate from '../../lib/is-save-data.js'
 
+function init () {
+  navigator.serviceWorker.register('/service.js')
+}
+
 if (process.env.NODE_ENV === 'production') {
   if (!isSaveDate() && navigator.serviceWorker) {
-    navigator.serviceWorker.register('/service.js')
+    init()
+  } else if (localStorage.appinstalled) {
+    init()
+  } else {
+    window.addEventListener('appinstalled', () => {
+      localStorage.appinstalled = true
+      init()
+    })
   }
 }
