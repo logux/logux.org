9ea5e343b4b743544f4769835601695fecd9baec
Andrey Sitnik <andrey@sitnik.ru>
2019-12-22 07:16:29 -0500

    Add submenu to branding

--- scripts/steps/build-pages.js
+++ scripts/steps/build-pages.js
@@ -1,7 +1,9 @@
 import { promises as fs } from 'fs'
 import rehypeParse from 'rehype-parse'
+import unistVisit from 'unist-util-visit'
 import capitalize from 'capitalize'
 import { join } from 'path'
+import slugify from 'slugify'
 import unified from 'unified'
 import pug from 'pug'
 
@@ -10,6 +12,17 @@ import { SRC, DIST } from '../lib/dirs.js'
 
 const PAGES = ['branding']
 
+function findHeaders (tree) {
+  let list = []
+  unistVisit(tree, 'element', node => {
+    if (node.tagName === 'h2') {
+      let text = node.children[0].value
+      list.push({ text, link: '#' + slugify(text, { lower: true }) })
+    }
+  })
+  return list
+}
+
 async function buildPages (assets, layout) {
   await PAGES.map(async i => {
     let filename = join(SRC, `${ i }.pug`)
@@ -17,7 +30,8 @@ async function buildPages (assets, layout) {
     let title = capitalize(i)
     let inner = pug.render(template.toString(), { filename })
     let tree = await unified().use(rehypeParse).parse(inner)
-    let html = await layout(`/${ i }/`, [], title, tree)
+    let submenu = findHeaders(tree)
+    let html = await layout(`/${ i }/`, submenu, title, tree)
     let dest = join(DIST, i, 'index.html')
     await fs.writeFile(dest, html)
     assets.add(dest)
