6cdf4277a940fa1d9d5cf874416e3f4ee05db6c8
Andrey Sitnik <andrey@sitnik.ru>
2019-12-29 23:27:30 -0500

    Fix cleaning before build

--- scripts/steps/clean-build-dir.js
+++ scripts/steps/clean-build-dir.js
@@ -5,7 +5,7 @@ import wrap from '../lib/spinner.js'
 import { DIST } from '../lib/dirs.js'
 
 async function cleanBuildDir () {
-  await del(join(DIST, '*'))
+  await del(join(DIST, '*'), { dot: true })
 }
 
 export default wrap(cleanBuildDir, 'Cleaning old files')
