c99513b7cf45ee3e8c4f17b43adb051742fec4fe
Andrey Sitnik <andrey@sitnik.ru>
2020-02-03 18:05:39 -0800

    Send HEAD tick only for HTML pages

--- src/service.js
+++ src/service.js
@@ -4,7 +4,9 @@ async function fromCache (e) {
   let cache = await caches.open(CACHE_VERSION)
   let match = await cache.match(e.request, { ignoreSearch: true })
   if (match) {
-    e.waitUntil(fetch(new Request(e.request, { method: 'HEAD' })))
+    if (e.request.url.endsWith('/')) {
+      e.waitUntil(fetch(new Request(e.request, { method: 'HEAD' })))
+    }
     return match
   } else {
     return fetch(e.request)
