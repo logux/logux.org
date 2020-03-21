0d215578eee0116ad8ddc263213a92f4049a6ae4
Andrey Sitnik <andrey@sitnik.ru>
2020-03-21 19:28:43 -0400

    Better spinner output on error

--- scripts/lib/spinner.js
+++ scripts/lib/spinner.js
@@ -9,11 +9,15 @@ if (process.env.CI || process.env.GITHUB_ACTIONS) {
   step = text => {
     process.stdout.write(`- ${ text }\n`)
     let start = Date.now()
-    return () => {
-      let time = (Date.now() - start + ' ms')
-      process.stdout.write(
-        chalk.green('✓ ') + text + ' ' + chalk.gray(time) + '\n'
-      )
+    return e => {
+      if (e) {
+        process.stdout.write(chalk.red('✖ ') + text + '\n')
+      } else {
+        let time = (Date.now() - start + ' ms')
+        process.stdout.write(
+          chalk.green('✓ ') + text + ' ' + chalk.gray(time) + '\n'
+        )
+      }
     }
   }
 } else {
@@ -22,25 +26,39 @@ if (process.env.CI || process.env.GITHUB_ACTIONS) {
     let id = lastId.toString()
     let start = Date.now()
     spinner.add(id, { text })
-    return () => {
-      let time = Date.now() - start
-      spinner.succeed(id, { text: text + chalk.gray(` ${ time } ms`) })
+    return e => {
+      if (e) {
+        spinner.fail(id)
+      } else {
+        let time = Date.now() - start
+        spinner.succeed(id, { text: text + chalk.gray(` ${ time } ms`) })
+      }
     }
   }
 }
 
 export async function run (text, fn) {
   let end = step(text)
-  let result = await fn()
-  end()
-  return result
+  try {
+    let result = await fn()
+    end()
+    return result
+  } catch (e) {
+    end(e)
+    throw e
+  }
 }
 
 export default function wrap (fn, text) {
   return async (...args) => {
     let end = step(text)
-    let result = await fn(...args)
-    end()
-    return result
+    try {
+      let result = await fn(...args)
+      end()
+      return result
+    } catch (e) {
+      end(e)
+      throw e
+    }
   }
 }
