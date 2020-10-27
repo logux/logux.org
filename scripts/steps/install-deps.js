euaaaio/logux.io

Add script to build Django API

scripts/steps/install-deps.js
@@ -22,15 +22,22 @@ export default async function installTypes (nextSteps) {
     await Promise.all(nextSteps())
   } else {
     await Promise.all(nextSteps())
-    await run('Installing types dependecies', async () => {
-      for (let i of dirs) {
-        await exec('yarn install --production', { cwd: i })
-        if (i.endsWith('logux-redux')) {
-          await exec('yarn add redux', { cwd: i })
-        } else if (i.endsWith('logux-vuex')) {
-          await exec('yarn add vuex', { cwd: i })
+    await Promise.all([
+      run('Installing Python dependecies', async () => {
+        await exec('make venv && make install', {
+          cwd: join(PROJECTS, 'logux-django')
+        })
+      }),
+      run('Installing types dependecies', async () => {
+        for (let i of dirs) {
+          await exec('yarn install --production', { cwd: i })
+          if (i.endsWith('logux-redux')) {
+            await exec('yarn add redux', { cwd: i })
+          } else if (i.endsWith('logux-vuex')) {
+            await exec('yarn add vuex', { cwd: i })
+          }
         }
-      }
-    })
+      })
+    ])
   }
 }

logux/logux.org

scripts/steps/install-deps.js
@@ -22,10 +22,17 @@ export default async function installTypes (nextSteps) {
     await Promise.all(nextSteps())
   } else {
     await Promise.all(nextSteps())
-    await run('Installing types dependecies', async () => {
-      for (let dir of dirs) {
-        await exec('yarn install --production=false', { cwd: dir })
-      }
-    })
+    await Promise.all([
+      run('Installing Python dependecies', async () => {
+        await exec('make venv && make install', {
+          cwd: join(PROJECTS, 'logux-django')
+        })
+      }),
+      run('Installing types dependecies', async () => {
+        for (let dir of dirs) {
+          await exec('yarn install --production=false', { cwd: dir })
+        }
+      })
+    ])
   }
 }