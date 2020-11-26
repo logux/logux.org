45d0aafe44fb208bce995c914970596b5aafc201
Andrey Sitnik <andrey@sitnik.ru>
2020-11-25 19:16:00 -0500

    Fix manifest generator

--- scripts/steps/generate-web-manifest.js
+++ scripts/steps/generate-web-manifest.js
@@ -1,5 +1,5 @@
 import { promises as fs } from 'fs'
-import { basename, join } from 'path'
+import { join } from 'path'
 
 import { SRC, DIST } from '../lib/dirs.js'
 import wrap from '../lib/spinner.js'
@@ -9,8 +9,18 @@ async function generateWebManifest (assets) {
   let json = JSON.parse(
     await fs.readFile(join(SRC, 'base', 'manifest.webmanifest'))
   )
-  json.icons[0].src = '/' + basename(assets.find(/192\..*\.png$/))
-  json.icons[1].src = '/' + basename(assets.find(/512\..*\.png$/))
+  let sizes = ['192', '512']
+  let icons = await Promise.all(
+    sizes.map(async size => {
+      let content = await fs.readFile(join(SRC, 'base', `${size}.png`))
+      let hashed = `${size}.${hash(content)}.png`
+      await fs.writeFile(join(DIST, hashed), content)
+      assets.add(join(DIST, hashed))
+      return `/${hashed}`
+    })
+  )
+  json.icons[0].src = icons[0]
+  json.icons[1].src = icons[1]
   let text = JSON.stringify(json)
   let name = `manifest.${hash(text)}.webmanifest`
   await fs.writeFile(join(DIST, name), text)
