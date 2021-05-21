c3b19ded56b7eb8a00d13f9045a98882653eb4b6
Eduard Aksamitov <e@edaks.co>
2021-05-22 00:34:39 +0300

    Fix API page semantics and margins on mobile (#32)
    * fix: header semantics on api page
    
    * fix: increase `h2` top margin on mobile

--- scripts/steps/create-layout.js
+++ scripts/steps/create-layout.js
@@ -219,11 +219,14 @@ function converter() {
         } else {
           node.properties.className = ['code']
         }
-      } else if (/^h[1-3]$/.test(node.tagName) && !node.properties.className) {
+      } else if (/^h[1-3]$/.test(node.tagName)) {
         visit(node, 'element', i => {
           if (i.tagName === 'code') i.noClass = true
         })
-        node.properties.className = ['title']
+        node.properties.className = [
+          'title',
+          ...(node.properties.className || [])
+        ]
         if (!node.noSlug) {
           let slug = node.slug ? node.slug : toSlug(node.children)
           node.properties.id = slug
