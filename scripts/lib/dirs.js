3cc61b9c184ad878876e54381d07ab17517a6357
Andrey Sitnik <andrey@sitnik.ru>
2024-08-07 01:05:00 +0200

    Clean up code

--- scripts/lib/dirs.js
+++ scripts/lib/dirs.js
@@ -1,8 +1,6 @@
-import { dirname, join } from 'path'
+import { join } from 'path'
 
-let self = new URL(import.meta.url).pathname
-
-export const ROOT = join(dirname(self), '..', '..')
+export const ROOT = join(import.meta.dirname, '..', '..')
 export const PROJECTS = join(ROOT, '..')
 export const DIST = join(ROOT, 'dist')
 export const SRC = join(ROOT, 'src')
