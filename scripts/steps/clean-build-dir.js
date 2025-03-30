db4990059effa7a375d404cc1cf8f9b4e40291f6
Andrey Sitnik <andrey@sitnik.ru>
2025-03-30 19:44:30 +0000

    Replace other part of dependencies and remove del

--- scripts/steps/clean-build-dir.js
+++ scripts/steps/clean-build-dir.js
@@ -1,11 +1,11 @@
-import { deleteAsync } from 'del'
+import { rm } from 'node:fs/promises'
 import { join } from 'node:path'
 
 import { DIST } from '../lib/dirs.js'
 import wrap from '../lib/spinner.js'
 
 async function cleanBuildDir() {
-  await deleteAsync(join(DIST, '*'), { dot: true })
+  await rm(join(DIST), { force: true, recursive: true })
 }
 
 export default wrap(cleanBuildDir, 'Cleaning old files')
